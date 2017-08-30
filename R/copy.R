582e8c458603a104b591d51d5c037435bbfd6de7
Alex M Chubaty <alexander.chubaty@canada.ca>
2017-08-30 13:18:43 +0000

    version 0.1.0

---
+++ R/copy.R
@@ -0,0 +1,46 @@
+if (!isGeneric("Copy")) {
+  setGeneric("Copy", function(object, ...) {
+    standardGeneric("Copy")
+  })
+}
+
+#' Copy for simList class objects
+#'
+#' Because a simList works with an environment to hold all objects,
+#' all objects within that slot are pass-by-reference. That means
+#' it is not possible to simply copy an object with an assignment operator:
+#' the two objects will share the same objects. As one simList object changes
+#' so will the other. when this is not the desired behaviour, use this function.
+#' NOTE: use capital C, to limit confusion with \code{data.table::copy()}
+#' See \code{\link[reproducible]{Copy}}.
+#'
+#' @inheritParams reproducible::Copy
+#' @param objects  Whether the objects contained within the simList environment
+#'                 should be copied. Default \code{TRUE}, which may be slow.
+#' @param queues Logical. Should the events queues (\code{events},
+#'               \code{current}, \code{completed}) be deep copied via
+#'               \code{data.table::copy}
+#'
+#' @author Eliot McIntire
+#' @exportMethod Copy
+#' @export
+#' @importFrom reproducible Copy
+#' @importMethodsFrom reproducible Copy
+#' @include simList-class.R
+#' @rdname Copy
+#' @seealso \code{\link[reproducible]{Copy}}
+setMethod("Copy",
+          signature(object = "simList"),
+          definition = function(object, objects, queues) {
+            if (missing(objects)) objects <- TRUE
+            if (missing(queues)) queues <- TRUE
+            sim_ <- object
+            if (queues) {
+              sim_@events <- data.table::copy(object@events)
+              sim_@current <- data.table::copy(object@current)
+            }
+            if (objects) {
+              sim_@.envir <- Copy(sim_@.envir)
+            }
+            return(sim_)
+})
---
+++ R/zzz.R
@@ -0,0 +1,52 @@
+## be sure to update the 'Package Options' section of the package help file
+##   in R/spades-core-package.R
+##
+.onLoad <- function(libname, pkgname) {
+  tmpdir <- file.path(tempdir(), "SpaDES")
+  ## set options using the approach used by devtools
+  opts <- options()
+  opts.spades <- list( # nolint
+    spades.cachePath = file.path(tmpdir, "cache"),
+    spades.inputPath = file.path(tmpdir, "inputs"),
+    spades.lowMemory = FALSE,
+    spades.modulePath = file.path(tmpdir, "modules"),
+    spades.moduleRepo = "PredictiveEcology/SpaDES-modules",
+    spades.nCompleted = 10000L,
+    spades.outputPath = file.path(tmpdir, "outputs"),
+    spades.tolerance = .Machine$double.eps ^ 0.5,
+    spades.useragent = "http://github.com/PredictiveEcology/SpaDES"
+  )
+  toset <- !(names(opts.spades) %in% names(opts))
+  if (any(toset)) options(opts.spades[toset])
+
+  invisible()
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (interactive()) {
+    packageStartupMessage("Using SpaDES.core version ", packageVersion("SpaDES.core"), ".")
+    packageStartupMessage("Default paths for SpaDES directories set to:\n",
+                          "  cachePath:  ", getOption("spades.cachePath"), "\n",
+                          "  inputPath:  ", getOption("spades.inputPath"), "\n",
+                          "  modulePath: ", getOption("spades.modulePath"), "\n",
+                          "  outputPath: ", getOption("spades.outputPath"), "\n",
+                          "These can be changed using 'setPaths()'. See '?setPaths'.")
+  }
+}
+
+.onUnload <- function(libpath) {
+  ## if temp session dir is being used, ensure it gets reset each session
+  tmpdir <- file.path(tempdir(), "SpaDES")
+  if (getOption("spades.cachePath") == file.path(tmpdir, "cache")) {
+    options(spades.cachePath = NULL)
+  }
+  if (getOption("spades.inputPath") == file.path(tmpdir, "inputs")) {
+    options(spades.inputPath = NULL)
+  }
+  if (getOption("spades.modulePath") == file.path(tmpdir, "modules")) {
+    options(spades.modulePath = NULL)
+  }
+  if (getOption("spades.outputPath") == file.path(tmpdir, "outputs")) {
+    options(spades.outputPath = NULL)
+  }
+}
